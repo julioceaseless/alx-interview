56b96b4a307bb38654ac4ef2d8b89b519e79b1d2
julioceaseless <julioceaseless@gmail.com>
2024-06-28 00:36:51 +0000

    Pascal's Triangle Project

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""A function to generate Pascal triangle"""
+
+
+def pascal_triangle(n):
+    """
+    generate pascal triangle
+    """
+    triangle = []
+
+    # return empty triangle if n <= 0
+    if n <= 0:
+        return pascal
+
+    # generate triangle
+    for i in range(n):
+        row = [1]
+        if i > 0:
+            for j in range(1, i):
+                # Each element is the sum of the two elements above it
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            # end each row with a 1
+            row.append(1)
+        triangle.append(row)
+
+    return triangle
