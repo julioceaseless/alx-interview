13f83154acd1241d9067cc69d29518a67e6406ea
julioceaseless <julioceaseless@gmail.com>
2024-07-14 13:31:45 +0000

    Interview question 2

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Interview question
+"""
+
+def minOperations(n):
+    """
+    calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+    """
+    factors = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors += divisor
+            n //= divisor
+        divisor += 1
+    return factors
