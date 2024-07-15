c61f34ee1bd2da9c12c61e45f30bfa6e9958fac5
julioceaseless <julioceaseless@gmail.com>
2024-07-15 20:14:50 +0000

    Fix 'index ouf of range' error

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,11 +14,15 @@ def canUnlockAll(boxes):
     keys = [0]
 
     while keys:
+        # Get a key from the list of keys we have
         key = keys.pop()
         if key not in unlocked_boxes:
+            # Mark the corresponding box as unlocked
             unlocked_boxes.add(key)
-            # Add all keys found in the current box to the keys list
-            keys.extend(boxes[key])
+            # Add all keys found in this box to our list of keys
+            for new_key in boxes[key]:
+                if new_key < len(boxes) and new_key not in unlocked_boxes:
+                    keys.append(new_key)
 
-    # Check if we have unlocked all boxes
+    # Check if we've unlocked all boxes
     return len(unlocked_boxes) == len(boxes)
