c2fcaf66b5769e565e32d170ceec0f44ff2cc856
julioceaseless <julioceaseless@gmail.com>
2024-08-09 16:54:51 +0300

    fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,18 +5,15 @@ const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 if (!movieId) {
-  console.log('Please provide a Movie ID');
   process.exit(1);
 }
 
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error('Error fetching movie data:', error);
     return;
   }
 
   if (response.statusCode !== 200) {
-    console.error('Failed to retrieve movie. Status code:', response.statusCode);
     return;
   }
 
@@ -26,7 +23,6 @@ request(apiUrl, (error, response, body) => {
   characters.forEach((characterUrl) => {
     request(characterUrl, (error, response, body) => {
       if (error) {
-        console.error('Error fetching character data:', error);
         return;
       }
 
